7ad7983df4b0e4043cf39723b13f86fdb28f2543
paupalou <paupalou@gmail.com>
2020-12-06 15:36:04 +0100

    Add Cart routes

--- server/routes.ts
+++ server/routes.ts
@@ -1,5 +1,8 @@
 import { Application } from 'express';
 import productsRouter from './api/controllers/products/router';
+import cartRouter from './api/controllers/cart/router';
+
 export default function routes(app: Application): void {
   app.use('/api/v1/products', productsRouter);
+  app.use('/api/v1/cart', cartRouter);
 }
